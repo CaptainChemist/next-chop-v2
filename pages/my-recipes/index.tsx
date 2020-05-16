e2aaae84d0fc3479d947891612526640112c7028
Stephen Jensen <stephen.jensen3@gmail.com>
2020-05-16 17:25:22 -0500

    27. Add Create Recipe Page

--- pages/my-recipes/index.tsx
+++ pages/my-recipes/index.tsx
@@ -1,20 +1,24 @@
 import styled from 'styled-components';
 import { MainLayout } from '../../components/layout/MainLayout';
 import { RecipesList, queryEnum } from '../../components/RecipesList';
-import { Col, Row } from 'antd';
+import { Col, Row, Button } from 'antd';
 import { useFetchUser } from '../../utils/user';
 import * as _ from 'lodash';
 import Router from 'next/router';
 import { Loading } from '../../components/notify/Loading';
+import Link from 'next/link';
 
 const StyledRow = styled(Row)`
   ${({ theme }) => `
+  div {
     padding: ${theme['padding-small']} ${theme['padding-small']};
     display: flex;
     h1 {
         padding-left: ${theme['padding-small']};
         text-align: left;
     }
+  }
+
   `}
 `;
 
@@ -31,7 +35,10 @@ const Index = () => {
   return (
     <MainLayout title="My Recipes">
       <StyledRow>
-        <Col>
+        <Col span={24}>
+          <Link href="/create">
+            <Button type="primary">Create</Button>
+          </Link>
           <h1>My Recipes</h1>
         </Col>
       </StyledRow>
