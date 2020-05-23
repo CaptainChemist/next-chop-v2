8db5dfaa701ba4f15a3c5c629f508afa0bf596f3
Stephen Jensen <stephen.jensen3@gmail.com>
2020-05-23 13:27:29 -0500

    36. Add Update Recipe Page

---
+++ pages/my-recipes/[id].tsx
@@ -0,0 +1,16 @@
+import { MainLayout } from '../../components/layout/MainLayout';
+
+const MyRecipe = ({ id }) => {
+  return (
+    <MainLayout title="Update Recipe">
+      <h1>Update Recipe</h1>
+    </MainLayout>
+  );
+};
+
+MyRecipe.getInitialProps = ({ query }) => {
+  const { id } = query;
+  return { id };
+};
+
+export default MyRecipe;
